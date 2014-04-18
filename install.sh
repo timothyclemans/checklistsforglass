3fce2e0f1700373991dfe91e13e694b30489e25b
Tim Clemans <timothy.clemans@gmail.com>
2014-04-18 09:39:49 -0700

    remove apache

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash  
 
+# Remove apache web server
+sudo update-rc.d -f apache remove
+
+sudo apt-get install cherokee
+
+
 sudo apt-get install python-pip
 sudo apt-get install git
 sudo apt-get install python-flup
